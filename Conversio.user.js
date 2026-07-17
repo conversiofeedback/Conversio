460a29e8ad2fa7c3ea1e3acedf1ad810efe3825d
conversiofeedback <conversiofeedback@gmail.com>
2026-07-17 17:38:53 +0300

    Update Conversio.user.js

--- Conversio.user.js
+++ Conversio.user.js
@@ -6,8 +6,8 @@
 // @match        *://*/*
 // @grant        none
 // @run-at       document-end
-// @updateURL    https://github.com
-// @downloadURL  https://github.com
+// @updateURL    https://github.com/conversiofeedback/Conversio/blob/main/Conversio.user.js
+// @downloadURL  https://github.com/conversiofeedback/Conversio/blob/main/Conversio.user.js
 // ==/UserScript==
 
 (function() {
